a2fa2f4e2ee22431e3644ec97eae5acb417ae33a
Alexey Barnyshev <alexeibarnyshev@yandex.ru>
2026-04-26 11:51:51 +0500

    Extend Specification

--- Core/Intrefaces/ISpecification.cs
+++ Core/Intrefaces/ISpecification.cs
@@ -7,6 +7,8 @@ public interface ISpecification<T>
     Expression<Func<T, bool>>? Criteria { get; }
     Expression<Func<T, object>>? OrderBy { get; }
     Expression<Func<T, object>>? OrderByDesc { get; }
+    List<Expression<Func<T,object>>> Includes {get; }
+    List<string> IncludeStrings {get;} 
     bool IsDistinct { get; }
     int Skip { get; }
     int Take { get; }
